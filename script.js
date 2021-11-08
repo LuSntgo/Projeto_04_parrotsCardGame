ece9724f6ff8e98138b9cdb52f594ed553ba27f2
LuSntgo <lu.sntgo@gmail.com>
2021-11-08 13:20:11 -0300

    Mudanças layout e ação de click

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-
 //const recebe o array com todas as cartas
 const cartas = document.querySelectorAll('.card');
+// declaração de variaveis
+let flipCarta = false;
+let primeiraCarta;
+let segundaCarta;
+let bloqueio = false;
 
 
 //função para flip
 function viraCarta(){
     this.classList.toggle('flip');
+    if (!flipCarta) {
+        flipCarta = true;
+         primeiraCarta = this;
+       return
+        }
+        segundaCarta = this;
+        flipCarta = false;
+        deuMatch();
+    }
+
+function deuMatch(){
+     if (primeiraCarta.dataset.identifier === segundaCarta.dataset.identifier) {
+        desativeCarta();
+        return;
+    }
+    desviraCarta();
 }
+ function desviraCarta(){
+     setTimeout(() => {
+         primeiraCarta.classList.remove('flip');
+          segundaCarta.classList.remove('flip');
+         }, 1000);
+     }
+ function desativeCarta() {
+    primeiraCarta.removeEventListener('click', viraCarta)
+    segundaCarta.removeEventListener('click', viraCarta)
+ }
+
 //para cada item do array CARTAS uma ação (callback) vai ser executada
 //forEach recebe 3 parametros
 //O primeiro (carta) é o item a ser inteirado
